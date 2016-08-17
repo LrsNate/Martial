8f4be4572414be9a7d108dd5a57894d690b941a3
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2016-08-17 15:29:05 +0200

    Search box skeleton

--- app/components/search-box/search-box.component.js
+++ app/components/search-box/search-box.component.js
@@ -1,13 +1,19 @@
 'use strict';
 
-angular.module('myApp.searchBox').component({
+angular
+    .module('myApp.searchBox')
+    .component('searchBox', {
     templateUrl: 'components/search-box/search-box.template.html',
     bindings: {
         filters: '='
     },
-    controller: function () {
+    controller: [function () {
         this.deleteFilter = function (index) {
             this.filters.splice(index, 1);
         };
-    }
+
+        this.addFilter = function () {
+            this.filters.push({});
+        };
+    }]
 });
