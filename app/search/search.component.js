2eda9a888cdfb4d4a594d0f668cc2f85109cf26f
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2016-08-17 18:25:45 +0200

    Display table skeleton

--- app/search/search.component.js
+++ app/search/search.component.js
@@ -13,5 +13,17 @@ angular
         templateUrl: 'search/search.template.html',
         controller: [function () {
             this.filters = [];
+            this.works = [
+                {
+                    author: 'Martial',
+                    reference: 'I, 15',
+                    title: 'Ad Julium'
+                },
+                {
+                    author: 'Martial',
+                    reference: 'I, 16',
+                    title: 'Ad Avitum, de suo libro'
+                }
+            ];
         }]
     });
