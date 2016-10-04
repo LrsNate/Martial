42641c2305e1b0d3701c5d01f8c9ebdfcb226660
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2016-10-04 23:50:00 +0100

    Bugfix on changelog items

--- app/components/changelog-items/index.js
+++ app/components/changelog-items/index.js
@@ -4,7 +4,7 @@ import ChangelogImproveDirective from './changelog-improve.directive';
 import ChangelogRefactorDirective from './changelog-refactor.directive';
 
 export default angular.module('myApp.changelogItems', [])
-    .directive('changelogAdd', ChangelogAddDirective)
-    .directive('changelogFix', ChangelogFixDirective)
-    .directive('changelogImprove', ChangelogImproveDirective)
-    .directive('changelogRefactor', ChangelogRefactorDirective);
+    .directive('changelogAdd', () => ChangelogAddDirective)
+    .directive('changelogFix', () => ChangelogFixDirective)
+    .directive('changelogImprove', () => ChangelogImproveDirective)
+    .directive('changelogRefactor', () => ChangelogRefactorDirective);
