3129d1b4ba38595ffaf7c088f1195acd5113c6d6
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2016-10-09 10:27:54 +0100

    Build tweaks

---
+++ webpack.prod.config.js
@@ -0,0 +1,14 @@
+const config = require('./webpack.config.js');
+const webpack = require('webpack'); // eslint-disable-line import/no-extraneous-dependencies
+
+config.plugins.push(new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: '"production"',
+  },
+}));
+
+config.plugins.push(new webpack.optimize.UglifyJsPlugin({ sourceMap: false }));
+
+config.plugins.push(new webpack.optimize.OccurrenceOrderPlugin());
+
+module.exports = config;
