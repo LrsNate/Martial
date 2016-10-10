6836cc140891c45e7c888660adfa00bbc68e493a
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2016-10-10 13:45:09 +0100

    Fix weird incipit if empty work

--- app/filters/strings/incipit.filter.js
+++ app/filters/strings/incipit.filter.js
@@ -1,7 +1,8 @@
 export default () => (input, maxLength) => {
+  if (!input) return '';
   const newlineIndex = input.indexOf('\n');
   if (newlineIndex < 0 || newlineIndex > maxLength) {
-    return input.substring(0, maxLength);
+    return `${input.substring(0, maxLength)}...`;
   }
   return input.substring(0, newlineIndex);
 };
