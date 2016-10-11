465317eb37ba6e82fe456748316f18c111b1061f
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2016-10-11 14:41:37 +0100

    Keep indentation in Markdown

--- app/filters/strings/markdown.filter.js
+++ app/filters/strings/markdown.filter.js
@@ -1,6 +1,12 @@
 import Remarkable from 'remarkable';
 
+const _ = require('lodash');
+
 export default () => {
   const md = new Remarkable();
-  return input => md.render(input);
+  return (input) => {
+    let lines = input.split('\n');
+    lines = _.map(lines, s => s.replace(/^( +)/g, (match, p1) => '&nbsp;'.repeat(p1.length)));
+    return md.render(lines.join('\n'));
+  };
 };
