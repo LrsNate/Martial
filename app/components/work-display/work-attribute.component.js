dbd7a7ee46550fd45758ed3e215e83745be83a66
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2017-04-30 19:44:21 +0100

    Bugfix: work attribute list unfolding

--- app/components/work-display/work-attribute.component.js
+++ app/components/work-display/work-attribute.component.js
@@ -11,7 +11,7 @@ export default {
     value: '<',
   },
   controller: class {
-    constructor() {
+    $onInit() {
       if (_.isArray(this.value)) {
         this.value = this.value.join(', ');
       }
